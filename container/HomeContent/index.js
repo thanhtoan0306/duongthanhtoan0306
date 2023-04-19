0b462805537570b72d9e6762b4dc82520eac5abf
Toan Duong <toandt@nexlesoft.com>
2023-04-19 22:33:24 +0700

    default image

--- container/HomeContent/index.js
+++ container/HomeContent/index.js
@@ -1,10 +1,11 @@
 import React from 'react'
 
 const HomeContent = () => {
+    const mySelf = `Hi there, I'm web dev`;
     return (
         <div>
             <div>This is my home page</div>
-            <div>Hi there, I'm web dev</div>
+            <div>{mySelf}</div>
             <div>3 years in programming</div>
             <div>many project</div>
         </div>
