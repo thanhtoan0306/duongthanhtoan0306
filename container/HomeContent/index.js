80114117a658f251b6e0796ca9d968f8a64bc3cb
Toan Duong <toandt@nexlesoft.com>
2023-04-19 22:50:53 +0700

    style 1

--- container/HomeContent/index.js
+++ container/HomeContent/index.js
@@ -1,11 +1,15 @@
 import React from 'react'
+import './styles.module.scss'
+// import './style1.module.css'
+// import '@/styles/style2.module.css'
 
 const HomeContent = () => {
-    const mySelf = `Hi there, I'm web dev`;
+    const myIntro = `Hi there, my name is Duong Thanh Toan. I'm 25 years old and I'm a web developer`;
     return (
         <div>
             <div>This is my home page</div>
-            <div>{mySelf}</div>
+            <div>{myIntro}</div>
+            <div>image</div>
             <div>3 years in programming</div>
             <div>many project</div>
         </div>
--- pages/index.js
+++ pages/index.js
@@ -11,9 +11,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main >
-        <div>Duong Thanh Toan</div>
         <HomeContent/>
-
       </main>
     </>
   )
