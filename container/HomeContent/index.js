4f7c8d7a3c697b9c14661f316e8add9e38f09255
Toan Duong <toandt@nexlesoft.com>
2023-04-19 22:25:46 +0700

    add contetn

--- container/HomeContent/index.js
+++ container/HomeContent/index.js
@@ -4,6 +4,9 @@ const HomeContent = () => {
     return (
         <div>
             <div>This is my home page</div>
+            <div>Hi there, I'm web dev</div>
+            <div>3 years in programming</div>
+            <div>many project</div>
         </div>
     )
 }
