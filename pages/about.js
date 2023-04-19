49bbfcd1846a685de6ccdf984b63fc3347fb2adf
Toan Duong <toandt@nexlesoft.com>
2023-04-19 09:49:18 +0700

    test new

---
+++ pages/about.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const AboutPage = () => {
+    return (
+        <div>
+            <h1>About Us</h1>
+            <p>Welcome to our website! We are a team of developers who are passionate about building amazing web applications.</p>
+        </div>
+    )
+}
+
+export default AboutPage
