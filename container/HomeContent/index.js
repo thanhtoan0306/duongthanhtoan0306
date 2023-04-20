3e023bf606107e82c3b256aa9d1a89880beda586
Toan Duong <toandt@nexlesoft.com>
2023-04-21 00:02:50 +0700

    oke button

--- container/HomeContent/index.js
+++ container/HomeContent/index.js
@@ -1,64 +1,8 @@
 import React from "react";
-import styled from "styled-components";
-
-const HomePageWrapper = styled.div`
-  width: 100%;
-  height: 100vh;
-  padding: 10px;
-  position: relative;
-  display: flex;
-  justify-content: space-between;
-  flex-wrap: wrap;
-  /* font-size: 99px; */
-  .content-container {
-    flex-basis: 55%;
-    width: 55%;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    .content {
-      width: fit-content;
-      line-height: 2;
-      padding: 10px;
-      .myname {
-        width: fit-content;
-        font-size: 40px;
-        font-weight: 700;
-        background-color: black;
-        color: white;
-        padding: 10px 20px;
-        transform: skew(-20deg);
-      }
-      .job {
-        font-size: 30px;
-      }
-    }
-    @media (max-width: 800px) {
-      flex-basis: 100%; // 100% width when viewport is smaller than $breakpoint
-    }
-  }
-  .image-box {
-    width: 45%;
-    min-width: 500px;
-    flex-basis: 45%;
-    display: flex;
-    justify-content: flex-end;
-    align-items: end;
-    img {
-      /* height: 50%; */
-      width: 100%;
-      /* max-height: 100%; */
-      /* max-width: 400px; */
-    }
-    @media (max-width: 800px) {
-      flex-basis: 100%; // 100% width when viewport is smaller than $breakpoint
-    }
-    /* position: relative; */
-  }
-`;
+import { HomePageWrapper } from "./styles";
+import { DownloadIcon } from "@/public/custom-icons/custom-icons";
 
 const HomeContent = () => {
-  const myIntro = `Hi there, my name is Duong Thanh Toan. I'm 25 years old and I'm a web developer`;
   return (
     <HomePageWrapper>
       <div className="content-container">
@@ -67,7 +11,12 @@ const HomeContent = () => {
           <div className="job">My name is Duong Thanh Toan</div>
           <div>"I have 3 years of experience in coding, with a focus on frontend web development as my major."</div>
           <div className="cv-button">
-            <button>📄 MY CV</button>
+            <div>
+              <span className="prefix">
+                <DownloadIcon />
+              </span>
+              <span className="button-label"> MY CV</span>
+            </div>
           </div>
         </div>
       </div>
---
+++ container/HomeContent/styles.js
@@ -0,0 +1,73 @@
+import styled from "styled-components";
+
+export const HomePageWrapper = styled.div`
+  width: 100%;
+  height: 100vh;
+  padding: 10px;
+  display: flex;
+  justify-content: space-between;
+  flex-wrap: wrap;
+  .content-container {
+    flex-basis: 55%;
+    width: 55%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    .content {
+      width: fit-content;
+      line-height: 2;
+      padding: 10px;
+      .myname {
+        width: fit-content;
+        white-space: nowrap;
+        font-size: 40px;
+        font-weight: 700;
+        background-color: black;
+        color: white;
+        padding: 10px 20px;
+        transform: skew(-20deg);
+      }
+      .job {
+        font-size: 30px;
+      }
+      .cv-button > div {
+        margin-top: 10px;
+        padding: 5px 10px;
+        border: 1px gainsboro solid;
+        border-radius: 5px;
+        width: fit-content;
+        display: flex;
+        justify-content: space-between;
+        gap: 10px;
+        align-items: center;
+        height: 44px;
+
+        cursor: pointer;
+        .prefix {
+          /* border: 1px solid gainsboro;
+          border-radius: 50%; */
+          /* width: fit-content; */
+          /* padding: 0 10px; */
+          /* background-color: black; */
+        }
+      }
+    }
+    @media (max-width: 800px) {
+      flex-basis: 100%; // 100% width when viewport is smaller than $breakpoint
+    }
+  }
+  .image-box {
+    width: 45%;
+    min-width: 500px;
+    flex-basis: 45%;
+    display: flex;
+    justify-content: flex-end;
+    align-items: end;
+    img {
+      width: 100%;
+    }
+    @media (max-width: 800px) {
+      flex-basis: 100%; // 100% width when viewport is smaller than $breakpoint
+    }
+  }
+`;
