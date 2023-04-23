bdd3d1e1a1aa3ded4fc020d06066a08cb53285da
Toan Duong <toandt@nexlesoft.com>
2023-04-23 17:34:36 +0700

    main layout

--- container/StoriesContent/index.js
+++ container/StoriesContent/index.js
@@ -4,8 +4,18 @@ import { StoriesPageWrapper } from "./styles";
 const StoriesContent = () => {
   return (
     <StoriesPageWrapper>
-      <div>IMage</div>
-      <div>content</div>
+      <div className="stories-container">
+        <div className="year-box">
+          2023
+          <div className="line"></div>
+        </div>
+        <div className="stories-box">
+          <div className="image">
+            <img height={200} src="/images/pt11.png" />
+          </div>
+          <div className="content">Bubbles Spa Websites</div>
+        </div>
+      </div>
     </StoriesPageWrapper>
   );
 };
--- container/StoriesContent/styles.js
+++ container/StoriesContent/styles.js
@@ -4,4 +4,29 @@ export const StoriesPageWrapper = styled.div`
   width: 100%;
   height: 100vh;
   padding: 70px 10px 10px 10px;
+  /* background-color: red; */
+  .stories-container {
+    height: 500px;
+
+    display: flex;
+    .year-box {
+      flex-basis: 10%;
+      display: flex;
+      justify-content: center;
+      position: relative;
+
+      .line {
+        position: absolute;
+        border: none;
+        border-left: 1px dashed black;
+        top: 50px;
+        height: 100%;
+        width: 1px;
+      }
+    }
+    .stories-box {
+      flex-basis: 90%;
+      display: flex;
+    }
+  }
 `;
