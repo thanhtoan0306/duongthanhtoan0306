48fffb574e86b5251bc5cc226f02d531c0366a1a
Toan Duong <toandt@nexlesoft.com>
2023-04-23 18:11:29 +0700

    tam on

--- container/StoriesContent/index.js
+++ container/StoriesContent/index.js
@@ -6,14 +6,20 @@ const StoriesContent = () => {
     <StoriesPageWrapper>
       <div className="stories-container">
         <div className="year-box">
-          2023
           <div className="line"></div>
+          2023 Bubbles Spa Websites
         </div>
         <div className="stories-box">
           <div className="image">
-            <img height={200} src="/images/pt11.png" />
+            <img src="/images/pt11.png" />
           </div>
-          <div className="content">Bubbles Spa Websites</div>
+          <div className="content">
+            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever
+            since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only
+            five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the
+            release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker
+            including versions of Lorem Ipsum.
+          </div>
         </div>
       </div>
     </StoriesPageWrapper>
--- container/StoriesContent/styles.js
+++ container/StoriesContent/styles.js
@@ -4,29 +4,54 @@ export const StoriesPageWrapper = styled.div`
   width: 100%;
   height: 100vh;
   padding: 70px 10px 10px 10px;
+  display: flex;
+  justify-content: center;
   /* background-color: red; */
   .stories-container {
-    height: 500px;
+    width: 70%;
+    height: 200px;
 
     display: flex;
     .year-box {
-      flex-basis: 10%;
+      flex-basis: 30%;
       display: flex;
-      justify-content: center;
-      position: relative;
+      gap: 20px;
+      /* justify-content: center; */
+      /* position: relative;   */
 
       .line {
-        position: absolute;
+        /* position: absolute; */
         border: none;
         border-left: 1px dashed black;
-        top: 50px;
+        /* top: 50px; */
         height: 100%;
         width: 1px;
       }
     }
     .stories-box {
-      flex-basis: 90%;
+      flex-basis: 70%;
       display: flex;
+      margin: 20px;
+      /* padding: 20px; */
+      border: 1px solid gainsboro;
+      border-radius: 20px;
+      overflow: hidden;
+      .image {
+        position: relative;
+        img {
+          height: 100%;
+          /* border-radius: 20px; */
+          /* position: absolute;
+          top: 0;
+          left: 0; */
+          /* width: ;
+          height: 100%; */
+          object-fit: cover;
+        }
+      }
+      .content {
+        padding: 10px;
+      }
     }
   }
 `;
